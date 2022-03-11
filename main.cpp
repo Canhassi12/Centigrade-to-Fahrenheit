3f7aa401549e9df67c786dd87d2f09f14715a1e7
Canhassi <arthurcanhassi@gmail.com>
2022-03-11 17:08:38 -0300

    Basico

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+///Centigrade to Fahrenheit///
+/////////by Canhassi/////////
+
+int main()
+{
+   int c, f;
+
+   cout<<"write how many Centigrade it is\n" <<endl;
+   cin>> c; 
+
+   f = c * 1.8 + 32;
+
+   cout<<"in that moment it is "<< f <<" fahrenheit"<< endl;
+   
+
+
+
+
+
+   
+
+
+
+    return 0;
+}
