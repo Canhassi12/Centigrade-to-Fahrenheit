e305d7c96d645b7a4d52e5399108880702b35b13
Canhassi <arthurcanhassi@gmail.com>
2022-03-11 19:03:13 -0300

    convert c to f, and, f to c

--- main.cpp
+++ main.cpp
@@ -6,14 +6,62 @@ using namespace std;
 
 int main()
 {
-   int c, f;
+   int opc;
+   float c, f;
+  
+   
 
-   cout<<"write how many Centigrade it is\n" <<endl;
-   cin>> c; 
+  cout<<"select the option:" <<endl;
+  cout<<"1:Centigrades to Fahrenheit "<<endl;
+  cout<<"2:Fahrenheit to Centigrades "<<endl;
+  
+  cin>> opc;
 
-   f = c * 1.8 + 32;
+  if (c!=1,2)
+  {
+      cout <<"anata wa baka desuka?" <<endl;
+  }
+ 
+  switch (opc)
 
-   cout<<"in that moment it is "<< f <<" fahrenheit"<< endl;
+	case 1:
+    {
+		cout<< "you select the option one" << endl;
+
+        cout<< "how many centigrades it is?" <<endl;
+        cin>>c;
+
+        f = 1.8 * c + 32;
+
+        cout<< f <<" that is your value in fahrenheit" << endl;
+        
+		break;
+
+	case 2:
+        cout<< "you select the option two" << endl;
+
+        cout<< "how many farrenheit it is?" <<endl;
+        cin>>f;
+
+        c = (f - 32) / 1.8;
+       
+        cout<< c <<" that is your value in centigrade" << endl;
+		
+        break;
+    }
+  
+  
+  
+  
+  
+  
+   
+
+   
+  
+   
+
+   
    
 
 
